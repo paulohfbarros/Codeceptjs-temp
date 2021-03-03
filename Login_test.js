d2dd0c7ae59997b4f636b745411ccd1ec9b06a34
unknown <paulohfbarros@gmail.com>
2021-03-03 10:48:48 -0300

    first commit

---
+++ Login_test.js
@@ -0,0 +1,23 @@
+Feature('Login');
+
+Scenario('test something', async ({ I }) => {
+    I.amOnPage('/');
+    I.see('Olá, futuro residente!');
+    I.waitForElement('#email');
+    I.fillField('#email', 'paulo.barros@codengage.com');
+    I.fillField('#password', '123456');
+    I.click('button[type=submit]');
+    I.click({ css: '[data-testid=menu-initial-button]'});
+    I.click({ css: '[data-testid=rm-menu__go-to--account]'});
+    I.wait(8);
+            
+    I.scrollPageToBottom('[class=es-main-menu__content--scrollable es-main-menu__content--scrollable--element]');     
+           
+    I.click({ css: '[data-testid=rm_menu_my-account__go_to--leave-account]'})
+    
+    I.click('Sair');
+    I.see('Olá, futuro residente!');
+     
+
+
+});
